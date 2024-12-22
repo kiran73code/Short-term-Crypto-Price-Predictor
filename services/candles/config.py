5e0d58a9c87b62253290744949e0927199b76f19
Kiran CR <kirancr2000@gmail.com>
2024-12-22 23:08:42 +0530

    candles services initial api completed to convert the trade to candle

---
+++ services/candles/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    """pydantic class for loading configuration parametrs"""
+
+    model_config = SettingsConfigDict(
+        env_file='settings.env', env_file_encoding='utf-8'
+    )
+    kafka_broker_address: str
+    kafka_input_topic: str
+    kafka_output_topic: str
+    kafka_consumer_group: str
+    candle_seconds: int
+
+
+config = Config()
---
+++ services/candles/run.py
@@ -0,0 +1,123 @@
+from datetime import timedelta
+from typing import Any, List, Optional, Tuple
+
+from config import Config
+from loguru import logger
+from quixstreams import Application
+from quixstreams.models import TimestampType
+
+
+def custom_ts_extractor(
+    value: Any,
+    headers: Optional[List[Tuple[str, bytes]]],
+    timestamp: float,
+    timestamp_type: TimestampType,
+) -> int:
+    """
+    Specifying a custom timestamp extractor to use the timestamp from the message payload
+    instead of Kafka timestamp.
+    """
+    # logger.debug(f'Custom timestamp extractor: {value}')
+    return value['timestamp_ms']
+
+
+def init_candle(trade: dict) -> dict:
+    """
+    Initialize a candle with the first trade
+    """
+    # breakpoint()
+    return {
+        'open': trade['price'],
+        'high': trade['price'],
+        'low': trade['price'],
+        'close': trade['price'],
+        'volume': trade['volume'],
+        'timestamp_ms': trade['timestamp_ms'],
+    }
+
+
+def update_candle(candle: dict, trade: dict) -> dict:
+    """
+    Update the candle with the latest trade
+    """
+    # breakpoint()
+    candle['close'] = trade['price']
+    candle['high'] = max(candle['high'], trade['price'])
+    candle['low'] = min(candle['low'], trade['price'])
+    candle['volume'] += trade['volume']
+    candle['timestamp_ms'] = trade['timestamp_ms']
+    return candle
+
+
+def main(
+    kafka_broker_address: str,
+    kafka_input_topic: str,
+    kafka_output_topic: str,
+    kafka_consumer_group: str,
+    candle_seconds: int,
+):
+    """
+    3 steps:
+    1. Ingests trades from Kafka
+    2. Generates candles using tumbling windows and
+    3. Outputs candles to Kafka
+
+    Args:
+        kafka_broker_address (str): Kafka broker address
+        kafka_input_topic (str): Kafka input topic
+        kafka_output_topic (str): Kafka output topic
+        kafka_consumer_group (str): Kafka consumer group
+        candle_seconds (int): Candle seconds
+
+    Returns:
+        None
+    """
+    logger.info('Starting the candles service!')
+
+    # Initialize the Quix Streams application
+    app = Application(
+        broker_address=kafka_broker_address, consumer_group=kafka_consumer_group
+    )
+
+    # Define the input and output topics
+    input_topic = app.topic(
+        name=kafka_input_topic,
+        value_deserializer='json',
+        timestamp_extractor=custom_ts_extractor,
+    )
+
+    output_topic = app.topic(
+        name=kafka_output_topic,
+        value_serializer='json',
+    )
+
+    # Create a Streaming DataFrame from the input topic
+    sdf = app.dataframe(topic=input_topic)
+
+    # Aggregation of trades into candles using tumbling windows
+    sdf = (
+        # Define a tumbling window of 10 minutes
+        sdf.tumbling_window(timedelta(seconds=candle_seconds))
+        # Create a "reduce" aggregation with "reducer" and "initializer" functions
+        .reduce(reducer=update_candle, initializer=init_candle)
+    )
+
+    # Emmit the current candle
+    sdf = sdf.current()
+
+    # push the candle to the output topic
+    sdf = sdf.to_topic(topic=output_topic)
+
+    # Restart the app
+    app.run()
+
+
+if __name__ == '__main__':
+    config = Config()
+    main(
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_input_topic=config.kafka_input_topic,
+        kafka_output_topic=config.kafka_output_topic,
+        kafka_consumer_group=config.kafka_consumer_group,
+        candle_seconds=config.candle_seconds,
+    )
